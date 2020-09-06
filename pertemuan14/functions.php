47f9ddbfa1f5def4944057c7ea0b8662f29ee5c1
eyditye <eyditye@gmail.com>
2020-09-06 17:57:23 +0700

    Update functions.php

--- pertemuan14/functions.php
+++ pertemuan14/functions.php
@@ -66,4 +66,35 @@ function cari($keyword){
 	return query($query);
 }
 
+function registrasi($data){
+	global $conn;
+
+	$username = strtolower(stripcslashes($data["username"]));
+	$password = mysqli_real_escape_string($conn,$data["password"]);
+	$password2= mysqli_real_escape_string($conn,$data["password2"]);
+
+	$result=mysqli_query($conn,"SELECT username FROM user where username = '$username'");
+
+	if (mysqli_fetch_assoc($result)){
+		echo " <script>
+			alert('username sudah terdaftar');
+		</script>"; 
+		return false;
+	}
+	//cek password
+	if ($password!== $password2){
+		echo"<script>
+			alert('konfirmasi password tidak sesuai');
+		</script>";
+		return false;
+	}
+
+	//enkripsi password
+	$password = password_hash($password,PASSWORD_DEFAULT);
+
+	mysqli_query($conn, "INSERT INTO user VALUES('','$username' , '$password')");
+
+	return mysqli_affected_rows($conn);
+}
+
  ?>
